c7cfe764aee2d2d7ea754f9287baff29364b2bb9
Fadhlaoui Raed <raed.fadhlaoui@hotmail.com>
2023-04-27 11:56:36 +0100

    feat: Setup Cors + Implement Get superHeroApi

--- SuperHeroAPI/SuperHeroAPI/Program.cs
+++ SuperHeroAPI/SuperHeroAPI/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy => { policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyHeader();
+    }));
 
 var app = builder.Build();
 
@@ -15,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("SuperHeroOrigins");
 
 app.UseHttpsRedirection();
 
